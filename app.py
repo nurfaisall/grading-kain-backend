616a4820324c28f0e093202db56bde758f2cc678
nurfaisall <nurfaisal1504@gmail.com>
2025-12-04 15:13:43 +0700

    asd

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import services.init
 app = Flask(__name__)
 
 services.init.get_db()
-app.register_blueprint(main_bp,url_prefix='/main')
+app.register_blueprint(main_bp, url_prefix="/main")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
--- routes/main.py
+++ routes/main.py
@@ -1,13 +1,31 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 import services.init as init
 
-main_bp = Blueprint('main', __name__)
+main_bp = Blueprint("main", __name__)
 
-@main_bp.route('/main')
+
+@main_bp.route("/gradingByDay", methods=["GET"])
 def hello_world():
-    data = init .data[0]['data'].to_dict()
-    return jsonify({
-        "message": "Hello, World! This is a simple Flask app using Blueprints.",
-        "status": "success",
-        "data": data
-    })
+    option = request.args.get("option")
+    day = request.args.get("day")
+    df = []
+
+    for data in init.data:
+        if data["name"] == option:
+            df = data["data"]
+
+    if day is None:
+        day = df["TANGGAL"].max()
+
+    df = df[df["TANGGAL"] == day]
+    grade
+
+    result = {}
+    df = df.to_dict()
+    return jsonify(
+        {
+            "message": "Hello, World! This is a simple Flask app using Blueprints.",
+            "status": "success",
+            "data": {"df": df},
+        }
+    )
