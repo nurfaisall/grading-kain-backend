6456a5778062c0da7a39c0093924c42d5d0f29e8
nurfaisall <nurfaisal1504@gmail.com>
2025-12-05 05:26:23 +0700

    update

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from routes.main import main_bp
 import services.init
+from flask_cors import CORS
+
+
 
 app = Flask(__name__)
 
+CORS(app,resources={r"/*": {"origins": "http://localhost:5173"}})
+
 services.init.get_db()
 app.register_blueprint(main_bp, url_prefix="/main")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
--- routes/main.py
+++ routes/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 import services.init as init
+import config.utils as utils
 
 main_bp = Blueprint("main", __name__)
 
@@ -18,14 +19,43 @@ def hello_world():
         day = df["TANGGAL"].max()
 
     df = df[df["TANGGAL"] == day]
-    grade
-
     result = {}
-    df = df.to_dict()
+    result["total_sum"] = {}
+    df = df.replace({np.nan: None})
+    result["df"] = df.to_dict()
+    for i in utils.list_yard:
+        result["total_sum"][i] = df[i].sum()
     return jsonify(
         {
             "message": "Hello, World! This is a simple Flask app using Blueprints.",
             "status": "success",
-            "data": {"df": df},
+            "data": result,
         }
-    )
+    )   
+
+main_bp.route("/gradingByMonth", methods=["GET"])
+def hello_world():
+    option = request.args.get("option")
+    day = request.args.get("day")
+    df = []
+
+    for data in init.data:
+        if data["name"] == option:
+            df = data["data"]
+
+    if day is None:
+        day = df["TANGGAL"].max()
+
+    df = df[df["TANGGAL"] == day]
+    result = {}
+    result["total_sum"] = {}
+    result["df"] = df.to_dict()
+    for i in utils.list_yard:
+        result["total_sum"][i] = df[i].sum()
+    return jsonify(
+        {
+            "message": "Hello, World! This is a simple Flask app using Blueprints.",
+            "status": "success",
+            "data": result,
+        }
+    )   
