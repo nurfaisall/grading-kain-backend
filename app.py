04c1386fa9077a6bb03139f4a449b609e90e9a21
nurfaisall <nurfaisal1504@gmail.com>
2025-12-05 17:09:12 +0700

    now

--- app.py
+++ app.py
@@ -4,14 +4,12 @@ import services.init
 from flask_cors import CORS
 
 
-
 app = Flask(__name__)
 
-CORS(app,resources={r"/*": {"origins": "http://localhost:5173"}})
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
 services.init.get_db()
 app.register_blueprint(main_bp, url_prefix="/main")
-
 
 
 if __name__ == "__main__":
--- routes/main.py
+++ routes/main.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, jsonify, request
+import numpy as np
 import services.init as init
 import config.utils as utils
+import pandas as pd
 
 main_bp = Blueprint("main", __name__)
 
 
 @main_bp.route("/gradingByDay", methods=["GET"])
-def hello_world():
+def gradingByDay():
     option = request.args.get("option")
     day = request.args.get("day")
     df = []
@@ -31,31 +33,35 @@ def hello_world():
             "status": "success",
             "data": result,
         }
-    )   
+    )
 
-main_bp.route("/gradingByMonth", methods=["GET"])
-def hello_world():
+
+@main_bp.route("/gradingByMonth", methods=["GET"])
+def gradingByMonth():
     option = request.args.get("option")
-    day = request.args.get("day")
+    month = request.args.get("month")
     df = []
 
     for data in init.data:
         if data["name"] == option:
             df = data["data"]
 
-    if day is None:
-        day = df["TANGGAL"].max()
+    df["tahun_bulan"] = pd.to_datetime(df["TANGGAL"]).dt.strftime("%Y-%m")
+    if month is None:
+        month = df["tahun_bulan"].max()
 
-    df = df[df["TANGGAL"] == day]
+    df = df[df["tahun_bulan"] == month]
     result = {}
     result["total_sum"] = {}
+    df = df.replace({np.nan: None})
     result["df"] = df.to_dict()
     for i in utils.list_yard:
         result["total_sum"][i] = df[i].sum()
+
     return jsonify(
         {
             "message": "Hello, World! This is a simple Flask app using Blueprints.",
             "status": "success",
             "data": result,
         }
-    )   
+    )
